1377e6903a94002145ffd9bbca6edad62cbad507
Mac OSx <72740002+Usmanasghar147@users.noreply.github.com>
2022-02-23 14:49:23 +0500

    1st

---
+++ CoreDataProject/CoreDataProject/CoreDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataProjectApp.swift
+//  CoreDataProject
+//
+//  Created by Macbook Pro on 15/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
